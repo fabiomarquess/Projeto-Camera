c2fbaa16e77cc4f7b1c48d72165b4b6e2d399335
Fabio Marques <76482044+fabiomarquess@users.noreply.github.com>
2021-06-29 11:59:51 -0300

    Criando html e JS

---
+++ script.js
@@ -0,0 +1,13 @@
+function startVideoFromCamera(){
+
+    const specs = {video: {width: 1080, aspectRatio:21/9}}
+
+    navigator.mediaDevices.getUserMedia(specs).then(stream=>{
+        const videoElement = document.querySelector("#video")
+        videoElement.srcObject = stream
+    }).catch(error=>{console.log(error)})
+
+
+}
+
+window.addEventListener("DOMContentLoaded", startVideoFromCamera)
