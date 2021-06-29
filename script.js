6f8421fbae5719c6be1e6354a84020e65ce5777a
Fabio Marques <76482044+fabiomarquess@users.noreply.github.com>
2021-06-29 12:32:04 -0300

    atualizando

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function startVideoFromCamera(){
     const specs = {video: {width: 1080, aspectRatio:21/9}}
 
     navigator.mediaDevices.getUserMedia(specs).then(stream=>{
-        const videoElement = document.querySelector("#video")
+        const videoElement = document.querySelector("#video2")
         videoElement.srcObject = stream
     }).catch(error=>{console.log(error)})
 
